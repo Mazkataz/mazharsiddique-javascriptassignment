7b2e3c7e0c2165338778be854b4eb2af58ca945d
Mazhar <mazkataz@hotmail.com>
2017-08-13 18:49:32 -0400

    Javascript Assignment

---
+++ script.js
@@ -0,0 +1,108 @@
+//global variable to stop alerts
+
+
+function userid_validation(i, j) {
+
+    //take strUser from user txtbox
+
+
+    var strUser = document.getElementsByName("userid")[0].value;
+
+
+
+    if (!strUser.length) {
+        console.log("User Id Should not be Empty!");
+
+    } else if (strUser.length > j) {
+        console.log("username should be at most " + j + " characters ");
+    } else if (strUser.length < i) {
+        console.log("username is should be atleast " + i + " characters ");
+
+    }
+
+
+    //  document.getElementsByName("passid")[0].focus(); 
+
+
+}
+
+
+
+function firstfocus() {
+    document.getElementsByName("userid")[0].focus();
+
+}
+
+
+function passid_validation(x, y) {
+
+
+    let strPass = document.getElementsByName("passid")[0].value;
+
+    if (!strPass.length) {
+        console.log("User Password Should not be Empty")
+    } else if (strPass.length > y)
+        console.log("password should be shorter than " + y + " characters");
+
+    else if (strPass.length < x)
+        console.log("password should be atleast " + x + " characters");
+
+    //    document.getElementsByName("username")[0].focus(); focus function 
+
+}
+
+
+
+
+
+
+
+function allLetter() {
+
+    var letters = document.getElementsByName("username")[0].value;
+
+
+    var flag = true;
+
+    letters = letters.toUpperCase();
+    // iterate through string, see if any non-letters, break. 
+
+
+
+    for (let it = 0; it < letters.length; ++it) {
+        if (letters[it] < 'A' || letters[it] > 'Z') {
+            // console.log(letters[it]);            this is the letter that broke it
+            flag = false;
+            break;
+        }
+    }
+
+    if (!flag)
+        console.log("only use letters in your name please");
+
+    //  document.getElementsByName("email")[0].focus();
+
+
+}
+
+function ValidateEmail() {
+
+
+    let email = document.getElementsByName("email")[0].value;
+
+    /* This Regex was taken from StackOverflow. 
+        poster "Squirtle"
+    */
+
+    var emailMatch = /\S+@\S+\.\S+/.exec(email);
+
+    if (!emailMatch) {
+        console.log("please enter a valid email");
+    }
+
+
+
+
+
+
+}
